7ca8e0788f948cec4e44f6d9506bc6605ad830fd
Владимир Сизов <7580844@mail.ru>
2022-02-19 14:31:24 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int initialBalance = 100;
+        int incomingSum = 1000;
+        int bonus = (incomingSum >= 1000) ? (incomingSum / 100) : 0;
+        int finalBalance = initialBalance + incomingSum + bonus;
+        System.out.println("Ваш итоговый счет составляет " + finalBalance);
+        if(bonus>0) System.out.println("Ваш бонус составляет " + bonus + " рублей");
+    }
+}
